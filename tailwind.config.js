d1a09707e5fabeac14b5bf993050b19981b90cd0
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-11-04 22:22:10 +0100

    added components,pages,data folders and font family in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",  
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lexend: ['Lexend', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+        santa: ['Santa Catalina', 'cursive'],
+      },
+    },
   },
   plugins: [],
 }
