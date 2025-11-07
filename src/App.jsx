f512092b8205c09efe1af1fb3724f59108b0ee5e
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-11-07 13:23:29 +0100

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header"
+
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-blue-500 text-white text-3xl font-bold">
-      Tailwind is working! 
+    <div className="">
+    <Header />
     </div>
   )
 }
