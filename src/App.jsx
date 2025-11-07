b787f8f88216b5a123ac76d32741df11fe2eb729
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-11-07 14:49:56 +0100

    added bg color

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header"
 
 function App() {
   return (
-    <div className="flex h-screen justify-center bg-grayLight">
+    <div className="flex h-screen justify-center bg-[theme(colors.grayLight)]">
       <Header />
     </div>
   )
