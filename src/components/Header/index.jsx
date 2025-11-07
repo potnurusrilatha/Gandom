72ff4c08dfc40fdbdf5fefe013378209a85a50e1
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-11-07 23:56:17 +0100

    added logocomponent and navbarcomponet in the header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,14 @@
-import logo from "../../assets/Logo Gandom/Logo color.png";
+import Logo from "../Logo";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
     <header className="w-full max-w-[1440px] h-[112px] mx-auto flex items-center justify-between px-8 relative">
-      {/* Logo */}
-      <div className="flex items-center">
-        <a href="/">
-          <img
-            src={logo}
-            alt="LogoGandom"
-            className="h-[80px] w-auto object-contain"
-          />
-        </a>
-      </div>
+      <Logo />
+      <Navbar />
     </header>
   );
 };
 
 export default Header;
+
