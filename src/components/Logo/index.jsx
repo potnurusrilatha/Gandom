61a252a3c5015a5e043f3484709a2df3072c01a5
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-11-08 00:49:33 +0100

    added changes

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,14 +1,17 @@
-import logo from "../../assets/Logo Gandom/Logo color.png";
+
+import logo from "../../assets/Logo Gandom/Logo color.png"
 
 const Logo = () => {
   return (
-    <a href="/">
-      <img
-        src={logo}
-        alt="Logocolor"
-        className="h-[80px] w-auto object-contain"
-      />
-    </a>
+    <div className="flex items-center">
+      <a href="/">
+        <img
+          src={logo}
+          alt="LogoGandom"
+          className="h-[80px] w-auto object-contain"
+        />
+      </a>
+    </div>
   );
 };
 
