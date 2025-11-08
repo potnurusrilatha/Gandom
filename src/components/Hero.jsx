1553e963f097cbb20a36925cb2243aa307d81f45
Raana <raana.barimani@hyperisland.se>
2025-11-08 17:47:41 +0100

    fixed based on feedback

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import coverImg from "../.././src/assets/Images/1.Home Page/_Cover.png";
+import coverImg from "../assets/Images/1.Home Page/_Cover.png";
+import logo from "../assets/Logo Gandom/Logo white.png";
 
 const Hero = () => {
     return (
         <section
-            className="relative w-full h-[90vh] bg-cover bg-center flex items-center justify-center"
+            className="relative w-full h-[90vh] bg-cover bg-center flex items-center justify-end pr-16"
             style={{ backgroundImage: `url(${coverImg})` }}
         >
             <div className="absolute inset-0 bg-black/20" />
-            <div className="relative text-center text-white z-10">
-                <h1 className="text-5xl font-santa text-primary mb-4 tracking-wide">
-                    GANDOM
-                </h1>
+            <div className="relative z-10 text-right text-white flex flex-col items-end">
+                <img src={logo} alt="GANDOM Logo" className="w-64 mb-4" />
                 <p className="text-xl font-lexend">
                     Wholesome flavors, redefined.
                 </p>
