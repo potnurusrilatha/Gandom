62e61a4d4d4af303f6495f49349ad89322ea889b
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-11-08 18:10:15 +0100

    changes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,10 @@
-import Logo from "../Logo"
-import Navbar from "../Navbar"
+import LogoGandom from "../LogoGandom/index.jsx"
+import Navbar from "../Navbar/index.jsx"
 
 const Header = () => {
   return (
     <header className="w-full max-w-[1440px] h-[112px] mx-auto flex items-center justify-between px-8 relative">
-      <Logo />
+      <LogoGandom />
       <Navbar />
     </header>
   );
