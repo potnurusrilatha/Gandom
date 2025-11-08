e248747300af934470895d70b9574ff385e9eeb9
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-11-08 17:45:33 +0100

    removed transition

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,11 @@
-import logo from "../../assets/Logo Gandom/Logo color.png";
 import Navbar from "../Navbar";
 
 const Header = () => {
   return (
-    <header className="w-full max-w-[1440px] h-[112px] mx-auto flex items-center justify-between px-8 relative">
-      {/* Logo */}
-      <div className="flex items-center gap-20">
-        <a href="/">
-          <img
-            src={logo}
-            alt="LogoGandom"
-            className="h-[80px] w-auto object-contain"
-          />
-        </a>
+    <header className="flex w-full max-w-7xl items-center justify-between px-6 py-4">
         
         <Navbar />
-      </div>
+      
     </header>
   );
 };
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -22,26 +22,26 @@ const Navbar = () => {
             <a
               key={link.href}
               href={link.href}
-              className="font-inter text-[16px] text-black hover:font-bold hover:text-primary transition-all duration-200"
+              className="font-inter text-[16px] text-black hover:font-bold hover:text-primary"
             >
               {link.label}
             </a>
           ))}
         </nav>
 
-        <button className="p-2 rounded hover:text-primary transition">
+        <button className="p-2 rounded  hover:text-primary">
           <TiShoppingCart className="w-[27px] h-[24px] text-black" />
         </button>
       </div>
 
       {/* Mobile Hamburger + Cart */}
       <div className="md:hidden flex items-center gap-4">
-        <button className="p-2 rounded hover:text-primary transition">
+        <button className="p-2 rounded hover:text-primary">
           <TiShoppingCart className="w-[27px] h-[24px] text-black" />
         </button>
 
         <button
-          className="p-2 rounded hover:text-primary transition"
+          className="p-2 rounded hover:text-primary"
           onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
         >
           <RxHamburgerMenu className="w-[27px] h-[27px] text-black" />
@@ -55,7 +55,7 @@ const Navbar = () => {
             <a
               key={link.href}
               href={link.href}
-              className="font-inter text-[18px] text-black hover:font-bold hover:text-primary transition-all duration-200"
+              className="font-inter text-[18px] text-black hover:font-bold hover:text-primary"
               onClick={() => setMobileMenuOpen(false)}
             >
               {link.label}
