b60962bc8344ac2902623b2f06d57d951b30e523
Raana <raana.barimani@hyperisland.se>
2025-11-08 19:58:19 +0100

    fixed logo on header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,11 @@
-import LogoGandom from "../LogoGandom/index.jsx"
-import Navbar from "../Navbar/index.jsx"
+import LogoGandom from "../../assets/Logo Gandom/Logo color.png"
+//import Navbar from "../Navbar/index.jsx"
 
 const Header = () => {
   return (
     <header className="w-full max-w-[1440px] h-[112px] mx-auto flex items-center justify-between px-8 relative">
-      <LogoGandom />
-      <Navbar />
+      <img src={LogoGandom} alt="Gandom Logo" className="h-16" />
+      {/* <Navbar /> */}
     </header>
   );
 };
