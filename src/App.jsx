82d988dc4ffab52f33b64be66f0e158b430057d5
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-11-08 18:21:56 +0100

    removed header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-// import Header from "./components/Header"
+
 import Footer from "./components/Footer"
 
 function App() {
   return (
     <div className="flex h-screen justify-center bg-[theme(colors.grayLight)]">
-      {/* <Header /> */}
+      
       <Footer />
     </div>
   )
