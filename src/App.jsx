1d748481dc714e9fa73ed11c46051256678d61dc
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-11-08 16:43:54 +0100

    logo works now

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import Header from "./components/Header"
+
+import Logo from "./components/Logo"
 
 function App() {
   return (
     <div className="flex h-screen justify-center bg-[theme(colors.grayLight)]">
-      <Header />
+  
+      <Logo />
     </div>
   )
 }
