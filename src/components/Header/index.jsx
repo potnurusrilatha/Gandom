787dd5bf159fc85eb8a84f10b5bf9ce7c50f3e2c
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-11-09 01:12:27 +0100

    navbar included

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,11 @@
 import LogoGandom from "../../assets/Logo Gandom/Logo color.png"
-//import Navbar from "../Navbar/index.jsx"
+import Navbar from "../Navbar/index.jsx"
 
 const Header = () => {
   return (
     <header className="w-full max-w-[1440px] h-[112px] mx-auto flex items-center justify-between px-8 relative">
       <img src={LogoGandom} alt="Gandom Logo" className="h-16" />
-      {/* <Navbar /> */}
+      <Navbar />
     </header>
   );
 };
