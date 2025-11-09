e07a21cec1aaf685603114dd1bf1f9b73adf70c6
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-11-09 02:58:02 +0100

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import EventsSpecialpromotions from "./components/EventsSpecialpromotions";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
 
       {/* Main content */}
       <main className="flex-grow w-full max-w-[1440px] px-8 mx-auto">
-        {/* Main content goes here */}
+        <EventsSpecialpromotions />
       </main>
 
       {/* Footer */}
