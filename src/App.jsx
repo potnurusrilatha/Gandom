4ec80a096731c6cd8dd23be5100ec149fce7e0de
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-11-13 12:09:12 +0100

    resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,8 @@ import HealthySection from "./components/HealthyDishes";
 import EventsSpecialpromotions from "./components/EventsSpecialpromotions";
 import SoupsPage from "./pages/Dishes/SoupsPage/page.jsx";
 import DessertsPage from "./pages/Dishes/DessertsPage/page.jsx";
-<<<<<<< HEAD
 import WafflesPage from "./pages/Dishes/WaffelsPage/page.jsx";
-import GymPage from "./pages/Dishes/SidedishesPage/page.jsx";
+import SidedishesPage from "./pages/Dishes/SidedishesPage/page.jsx";
 import Delivery from "./components/Delivery";
 import DeliveryOrderPage from "./pages/Delivery/DeliveryOrderPage/page.jsx";
 import CreateBoxMenuPage from "./pages/Delivery/CreateBoxMenu/page.jsx";
@@ -14,10 +13,6 @@ import AboutUsPage from "./pages/AboutUs/about-us"
 
 
 import { Routes, Route } from "react-router-dom";
-=======
-import WaffelsPage from "./pages/Dishes/WaffelsPage/page.jsx";
-import SidedishesPage from "./pages/Dishes/SidedishesPage/page.jsx";
->>>>>>> T5/feature/Healthy-Dishes-Grid
 
 function App() {
   return (
@@ -28,10 +23,7 @@ function App() {
       {/* Main content */}
       <main className="flex-grow w-full max-w-[1440px] px-8 mx-auto">
         <Routes>
-<<<<<<< HEAD
           {/* Home page */}
-=======
->>>>>>> T5/feature/Healthy-Dishes-Grid
           <Route
             path="/"
             element={
@@ -43,13 +35,12 @@ function App() {
               </>
             }
           />
-<<<<<<< HEAD
 
           {/* Dishes pages */}
           <Route path="/dishes/soups" element={<SoupsPage />} />
           <Route path="/dishes/desserts" element={<DessertsPage />} />
           <Route path="/dishes/waffles" element={<WafflesPage />} />
-          <Route path="/dishes/gym" element={<GymPage />} />
+          <Route path="/dishes/sidedishesPage" element={<SidedishesPage />} />
 
           {/* Delivery pages */}
           <Route path="/delivery/order" element={<DeliveryOrderPage />} />
@@ -57,12 +48,6 @@ function App() {
           <Route path="/about-us" element={<AboutUsPage />} />
 
 
-=======
-          <Route path="/dishes/soupsPage" element={<SoupsPage />} />
-          <Route path="/dishes/dessertsPage" element={<DessertsPage />} />
-          <Route path="/dishes/waffelsPage" element={<WaffelsPage />} />
-          <Route path="/dishes/sidedishesPage" element={<SidedishesPage />} />
->>>>>>> T5/feature/Healthy-Dishes-Grid
         </Routes>
 
       </main>
