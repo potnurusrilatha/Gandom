89d6b3d43701fc69dcef9ae637c92781161b623f
Srilatha <119068383+potnurusrilatha@users.noreply.github.com>
2025-11-13 11:30:55 +0100

    added sidedishes name and image

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import EventsSpecialpromotions from "./components/EventsSpecialpromotions";
 import SoupsPage from "./pages/Dishes/SoupsPage/page.jsx";
 import DessertsPage from "./pages/Dishes/DessertsPage/page.jsx";
 import WaffelsPage from "./pages/Dishes/WaffelsPage/page.jsx";
-import GymPage from "./pages/Dishes/GymPage/page.jsx";
+import SidedishesPage from "./pages/Dishes/SidedishesPage/page.jsx";
 
 function App() {
   return (
@@ -17,7 +17,6 @@ function App() {
       {/* Main content */}
       <main className="flex-grow w-full max-w-[1440px] px-8 mx-auto">
         <Routes>
-          {/* 🏠 Home page (Healthy + Events sections) */}
           <Route
             path="/"
             element={
@@ -30,7 +29,7 @@ function App() {
           <Route path="/dishes/soupsPage" element={<SoupsPage />} />
           <Route path="/dishes/dessertsPage" element={<DessertsPage />} />
           <Route path="/dishes/waffelsPage" element={<WaffelsPage />} />
-          <Route path="/dishes/gymPage" element={<GymPage />} />
+          <Route path="/dishes/sidedishesPage" element={<SidedishesPage />} />
         </Routes>
       </main>
 
