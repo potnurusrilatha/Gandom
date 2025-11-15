02876d7a6a673fd63c7042367796893f03f53cd6
Sunil Kanumula <sunil.kanumula@gmail.com>
2025-11-15 19:32:47 +0100

    added data js in desserts

---
+++ src/data/desserts.js
@@ -0,0 +1,20 @@
+import SholeZard from "../assets/desserts/sholezard.png";   
+
+export const desserts = [
+  {
+    title: "Chocolate Cake",
+    price: "45 SEK",
+    description: "Rich chocolate cake with layers",
+    image: SholeZard,  
+    nutrition: [
+      { nutrient: "Calories", amount: "350 kcal", notes: "High energy dessert" },
+      { nutrient: "Protein", amount: "5 g", notes: "Contains eggs and milk" },
+      { nutrient: "Fat", amount: "20 g", notes: "Mostly from cocoa and butter" },
+      { nutrient: "Carbs", amount: "40 g", notes: "Sugar and flour" },
+      { nutrient: "Fiber", amount: "3 g", notes: "From cocoa" },
+      { nutrient: "Sugars", amount: "25 g", notes: "From sugar and chocolate" },
+      { nutrient: "Sodium", amount: "150 mg", notes: "From baking ingredients" },
+      { nutrient: "Micronutrients", amount: "-", notes: "Contains iron, calcium, antioxidants" },
+    ],
+  },
+];
